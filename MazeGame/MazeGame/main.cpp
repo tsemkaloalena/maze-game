3660c122cce425e4b61f32994da0d79bb3875777
ovsannikovav97@gmail.com <ovsannikovav97@gmail.com>
2021-03-31 12:54:37 +0300

    Меню

--- MazeGame/MazeGame/main.cpp
+++ MazeGame/MazeGame/main.cpp
@@ -1,9 +1,40 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <sstream>
 using namespace sf;
-
-int main() {
+void Menu(RenderWindow& window)
+{
+	Texture MenuTextureStart, MenuTextureExit;
+	MenuTextureStart.loadFromFile("Start.png");
+	MenuTextureExit.loadFromFile("Exit.png");
+	Sprite MenuStart(MenuTextureStart), MenuExit(MenuTextureExit);
+	bool isMenu = 1;
+	int MenuNum = 0;
+	MenuStart.setPosition(100, 30);
+	MenuExit.setPosition(100, 90);
+	while (isMenu)
+	{
+		MenuStart.setColor(Color::White);
+		MenuExit.setColor(Color::White);
+		MenuNum = 0;
+		window.clear(Color(129, 181, 221));
+		if (IntRect(100, 30, 300, 50).contains(Mouse::getPosition(window))) { MenuStart.setColor(Color::Blue); MenuNum = 1; }
+		if (IntRect(100, 90, 300, 50).contains(Mouse::getPosition(window))) { MenuExit.setColor(Color::Blue); MenuNum = 2; }
+		if (Mouse::isButtonPressed(Mouse::Left))
+		{
+			if (MenuNum == 1) isMenu = false;
+			if (MenuNum == 2) { window.close(); isMenu = false; }
+		}
+		window.draw(MenuStart);
+		window.draw(MenuExit);
+		window.display();
+	}
+}
+int main() 
+{
 	setlocale(0, "");
-	gghjd
+	RenderWindow window(VideoMode(350, 150), "SFML works!");
+	Menu(window);
 	return 0;
 }
